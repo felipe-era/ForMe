89852cfb4c4f9e765e1afaf0314a5b926413eb7a
feveloper <felip@FEVOLEPER>
2023-07-02 21:45:43 -0300

    IdeasAndTests

---
+++ ForMe/Controllers/SongController.cs
@@ -0,0 +1,73 @@
+﻿using ForMe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ForMe.Controllers;
+
+internal class SongController
+{
+    public async Task<string> ConectaApiAsync(string strRetorno)
+    {
+        strRetorno = string.Empty;
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage httpResponse = await client.GetAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+                httpResponse.EnsureSuccessStatusCode(); // Lança uma exceção se a requisição não for bem-sucedida (código de status 2xx)
+
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    // Lê o conteúdo da resposta como uma string
+                    string responseBody = await httpResponse.Content.ReadAsStringAsync();
+                    // Processa os dados recebidos
+                    var musicas = JsonSerializer.Deserialize<List<Music>>(responseBody);
+                    //rtbResponse.Text = musicas.Count.ToString();
+                    //musicas[0].ExibirDetalhesMusica();
+                    strRetorno = musicas[0].ToString();
+                }
+                else
+                {
+                    // A requisição não foi bem-sucedida, exibe o código de status
+                    MessageBox.Show($"Falha na requisição. Código de status: {httpResponse.StatusCode}");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Ocorreu um erro na requisição: {ex.Message}");
+            }
+        }
+        return strRetorno;
+
+    }
+
+    public async Task<string> ConectaApi2Async(string strRetorno)
+    {
+        strRetorno = string.Empty;
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                string strResponse = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+                strRetorno = strResponse;
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Ocorreu um erro na requisição: {ex.Message}");
+            }
+
+        }
+        return strRetorno;
+    }
+
+
+
+
+
+}
--- ForMe/Views/frmMusicas.cs
+++ ForMe/Views/frmMusicas.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using ForMe.Controllers;
+using ForMe.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,13 +15,24 @@ namespace ForMe.Views;
 
 public partial class frmMusicas : Form
 {
+    SongController songController = new SongController();
+
     public frmMusicas()
     {
         InitializeComponent();
     }
 
-    private void button1_Click(object sender, EventArgs e)
+
+    private async void btnTryHttp_Click(object sender, EventArgs e)
     {
+        string strTmp = songController.ConectaApiAsync("").ToString();
+        rtbResponse.Text = strTmp;
+    }
+
+    private void btnTryHttp2_Click(object sender, EventArgs e)
+    {
+        string strTmp = songController.ConectaApi2Async("").ToString();
+        rtbResponse.Text = strTmp;
 
     }
 }
